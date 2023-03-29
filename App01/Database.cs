a8de7d284943a1bf27265de6ce01c7f8a6a5079b
Lil s0uxz <its.s0ux@gmail.com>
2023-03-29 14:34:39 +0700

    Add project files.

---
+++ App01/Database.cs
@@ -0,0 +1,46 @@
+﻿/* Author: Lil s0uxz
+ * Github profile: https://github.com/lils0uxz
+ * Version: beta-1.0.1
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace App01
+{
+    class Database
+    {
+        // connect to mysql database
+        private MySqlConnection connection = new MySqlConnection("server=127.0.0.1;port=3306;username=root;password=0546;database=demohmiconnectpc");
+
+
+        // create a function to open the connection
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        // create a function to close the connection
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        // create a function to return the connection
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ App01/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("App01")]
+[assembly: AssemblyDescription("Connect to JP server to show databse & export to excel file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HondaplusVN")]
+[assembly: AssemblyProduct("App01")]
+[assembly: AssemblyCopyright("Copyright ©  2023 by Lil s0uxz")] // Link to the author: https://github.com/lils0uxz
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01d24588-29bb-4b9c-8f6e-6163c3590c3c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
