af0aba6e9de47ec1b91379e55172b0ba18f1c499
Lil s0uxz <its.s0ux@gmail.com>
2023-04-03 16:07:55 +0700

    misc

--- TestForm/Form1.cs
+++ TestForm/Form1.cs
@@ -28,42 +28,8 @@ namespace TestForm
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            // Validate the user's credentials
-            if (IsValidCredentials(txtUsername.Text, txtPassword.Text))
-            {
-                // Generate a new GUID for the user
-                Guid userId = Guid.NewGuid();
-
-                // Connect to the MySQL database
-                string connectionString = "server=localhost;database=mydatabase;uid=myusername;password=mypassword;";
-                using MySqlConnection connection = new MySqlConnection(connectionString);
-                connection.Open();
-
-                // Create a new MySQL command to insert the user's ID into the database
-                string query = "INSERT INTO users (id) VALUES (@id)";
-                using MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.AddWithValue("@id", userId);
-                command.ExecuteNonQuery();
-
-                // Disconnect from the MySQL database
-                connection.Close();
-
-                // Show the main form
-                Form2 form2 = new Form2(userId);
-                form2.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            
         }
-        private bool IsValidCredentials(string username, string password)
-            {
-                // Validate the user's credentials here
-                // Return true if the credentials are valid, false otherwise
-                return true;
-            }
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
